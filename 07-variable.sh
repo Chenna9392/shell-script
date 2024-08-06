19ab3acff02b36b403e2a7884f2be22bb8fdb601
paparaok <venkatk1218@gmail.com>
2024-08-06 09:47:02 +0530

    8th commit

--- 07-variable.sh
+++ 07-variable.sh
@@ -1,13 +1,13 @@
 #!bin/bash
 
-echo "Please enter your username::"
+echo "Please enter your username:: "
 
 read -s USERNAME # the value enter the above will be automatically attached to username variable
 
-echo "please enter your password ::"
+echo "please enter your password:: "
 read -s PASSWORD
 
-echo "Username is : $USERNAME,Password is : $PASSWORD" # I am printing just for validation, you should not print username and passwords in scripts
+echo "Username is: $USERNAME,Password is: $PASSWORD" # I am printing just for validation, you should not print username and passwords in scripts
 
 
 
